03f14f030117792a908e8566861fe0ad5dbc1a0e
Tanna <tanna.la.lynn@gmail.com>
2015-11-16 05:57:12 -0800

    started

--- AndroidProject/Biometrix/biometrixApp/src/main/java/com/rocket/biometrix/SelectDate_EmotionRater.java
+++ AndroidProject/Biometrix/biometrixApp/src/main/java/com/rocket/biometrix/SelectDate_EmotionRater.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,7 +24,11 @@ public class SelectDate_EmotionRater extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_date__emotion_rater);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
